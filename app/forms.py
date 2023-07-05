b422c5d8957b82439a456914b00c8cfda7d3f35a
Sebastian Parraguez <se.parraguez@duocuc.cl>
2023-07-05 01:12:37 -0400

    registro usuario

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Contacto, Producto
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate
+from .models import Cuenta
 
 class ContactoForm(forms.ModelForm):
     class Meta:
@@ -16,3 +19,34 @@ class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
         fields = ('nombre', 'precio', 'imagen')
+
+
+
+
+
+class RegistroForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Requerido. Ingresa un email válido.')
+
+    class Meta:
+        model = Cuenta
+        fields = ('email', 'password1', 'password2')
+
+
+class AutenticarForm(forms.ModelForm):
+    password = forms.CharField(label='contraseña', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Cuenta
+        fields = ['email', 'password']
+
+    def clean(self):
+        email = self.cleaned_data['email']
+        password = self.cleaned_data['password']
+
+        if not authenticate(email=email, password=password):
+            raise forms.ValidationError("El email o contraseña son incorrectos")
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(label='Email')
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
--- web/urls.py
+++ web/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+    
 ] 
 
 if settings.DEBUG:
