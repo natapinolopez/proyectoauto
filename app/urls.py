3ca33cf9a87888ae593a4c1a8e6f4f247dd4e66c
Sebastian Parraguez <se.parraguez@duocuc.cl>
2023-07-08 02:00:29 -0400

    TERMINO CARRITO DE COMPRAS JUNTO CON LAS VIEWS Y URLS

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, contacto, inicio, servicio, acercade
+from .views import home, contacto, inicio, servicio, acercade, agregar_al_carrito, ver_carrito, eliminar_del_carrito
 from . import views
 # from .views import login_view, registro, inicio_sesion, cerrar_sesion
 from .views import login_view, register_view, logout_view
@@ -9,6 +9,9 @@ urlpatterns = [
     path('contacto/', contacto, name='contacto'),
     path('inicio/', inicio, name="inicio"),
     path('servicio/', servicio, name="servicio"),
+    path('agregar-al-carrito/<int:producto_id>/', agregar_al_carrito, name='agregar_al_carrito'),
+    path('carrito/', ver_carrito, name='carrito'),
+    path('eliminar-del-carrito/<int:producto_id>/', eliminar_del_carrito, name='eliminar_del_carrito'),
     path('acercade/', acercade, name="acercade"),
     path('login/', login_view, name='login'),
     path('registro/', views.register_view, name='register'),
